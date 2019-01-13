a42bac4bd69d2068c52154764a971d337c0a482a
virgesmith <andrew@friarswood.net>
2019-01-13 19:46:55 +0000

    finish threadpool example

--- threadpool/src/lib.rs
+++ threadpool/src/lib.rs
@@ -14,19 +14,29 @@ impl<F: FnOnce()> FnBox for F {
   }
 }
 
+enum Message { NewJob(Job), Terminate, }
+
 struct Worker {
   id: usize,
   thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
-  fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+  fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
     //let thread = thread::spawn(|| { receiver; });
     let thread = thread::spawn(move || {
       loop {
-        let job = receiver.lock().unwrap().recv().unwrap();
-        println!("Worker {} got a job; executing.", id);
-        job.call_box();
+        let message = receiver.lock().unwrap().recv().unwrap();
+        match message {
+          Message::NewJob(job) => {
+            println!("Worker {} got a job; executing.", id);
+            job.call_box();
+          },
+          Message::Terminate => {
+            println!("Worker {} was told to terminate.", id);
+            break;
+          },
+        }
       }
     });
     Worker { id, thread: Some(thread), }
@@ -37,7 +47,7 @@ type Job = Box<FnBox + Send + 'static>;
 
 pub struct ThreadPool {
   workers: Vec<Worker>,
-  sender: mpsc::Sender<Job>,
+  sender: mpsc::Sender<Message>,
 }
 
 impl ThreadPool {
@@ -63,12 +73,17 @@ impl ThreadPool {
     where F: FnOnce() + Send + 'static
   {
     let job = Box::new(f);
-    self.sender.send(job).unwrap();
+    self.sender.send(Message::NewJob(job)).unwrap();
   }
 }
 
 impl Drop for ThreadPool {
   fn drop(&mut self) {
+    println!("Sending terminate message to all workers.");
+    for _ in &mut self.workers {
+      self.sender.send(Message::Terminate).unwrap();
+    }
+    println!("Shutting down all workers.");
     for worker in &mut self.workers {
       println!("Shutting down worker {}", worker.id);
       if let Some(thread) = worker.thread.take() {
@@ -77,6 +92,7 @@ impl Drop for ThreadPool {
     }
   }
 }
+
 #[cfg(test)]
 mod tests {
     #[test]
