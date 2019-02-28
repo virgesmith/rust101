e92ed01a1cebd37ca3aecce5f9b713361ec97eac
virgesmith <andrew@friarswood.net>
2019-02-28 21:42:57 +0000

    ...

--- app/src/main.rs
+++ app/src/main.rs
@@ -3,6 +3,12 @@ use rand::dist::*;
 
 fn main() {
   let mut rng = LCG::new();
-  let dist = Exponential::new(2.0);
-  println!("{}", dist.sample_1(&mut rng));
+  let mut dist = WithoutReplacement::new(&[(1,2),(2,2),(3,2),(4,2),(5,2),(6,2)]);
+  
+  while !dist.empty() {
+    println!("{}", dist.sample_1(&mut rng));
+  }
+  
+  let rng = Sobol::new(8);
+  println!("{:?}", rng.next_d())
 }
